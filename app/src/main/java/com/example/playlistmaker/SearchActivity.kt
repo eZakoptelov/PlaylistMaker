47f7a1a7b2cbd26ba86fa424e48d22cbdcf46ab7
eZakoptelov <dezakpw@gmail.com>
2026-03-16 14:02:30 +0500

    Плэйлист продолжение)

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,10 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
@@ -38,6 +38,7 @@ class SearchActivity : AppCompatActivity() {
         // Очистим текст при нажатии на кнопку
         clearButton.setOnClickListener {
             editText.text.clear()
+            hideKeyboard(editText)
         }
     }
 
@@ -60,6 +61,10 @@ class SearchActivity : AppCompatActivity() {
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         restoreSearchText(savedInstanceState)
+    }
+    private fun hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     companion object {
